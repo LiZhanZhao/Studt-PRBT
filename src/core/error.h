c3a7bd7375a99b432734ece0e178ea4ffa3b3d8a
lzz <120274270@qq.com>
2017-06-04 18:01:13 +0800

    删除测试工程

--- src/core/error.h
+++
@@ -1,9 +0,0 @@
-#ifndef PBRT_CORE_ERROR_H
-#define PBRT_CORE_ERROR_H
-
-void Info(const char *, ...);
-void Warning(const char *, ...);
-void Error(const char *, ...) ;
-void Severe(const char *, ...);
-
-#endif // PBRT_CORE_ERROR_H
--- src/core/renderer.cpp
+++
@@ -1,9 +0,0 @@
-
-// core/renderer.cpp*
-#include "renderer.h"
-
-// Renderer Method Definitions
-Renderer::~Renderer() {
-}
-
-
