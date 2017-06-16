d32cdecfe8d17e636f26d44a98282bfdacf2e791
lzz <120274270@qq.com>
2017-06-16 23:34:23 +0800

    添加注释，了解DifferentialGeometry的作用

--- src/core/diffgeom.h
+++ src/core/diffgeom.h
@@ -40,6 +40,16 @@
 #include "pbrt.h"
 #include "geometry.h"
 
+
+/*
+A self-contained representation for the geometry
+of a particular point on a surface (typically the point of a ray intersection).
+This abstraction needs to hide the particular type of geometric shape the point lies
+on, supplying enough information about the surface point to allow the shading and
+geometric operations in the rest of pbrt to be implemented generically, without the
+need to distinguish between different shape types such as spheres and triangles
+*/
+
 // DifferentialGeometry Declarations
 struct DifferentialGeometry {
     DifferentialGeometry() { 
